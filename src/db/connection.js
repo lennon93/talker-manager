fa7f6dd570641e07ca3e85e2ff8c330041fdaf0f
Lennon <lennon_lxs@hotmail.com>
2023-04-20 13:33:27 -0300

    Finaliza o projeto

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'db',
-  port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'TalkerDB',
+  host: process.env.MYSQL_HOSTNAME,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
